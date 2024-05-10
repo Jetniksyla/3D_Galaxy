6811150c1e6d5056450998c934ea218c1dfdee69
Jetnik Syla <sjetnik@gmail.com>
2024-05-10 01:03:54 -0400

    Add footer with copyright information ('2024 Jetnik Syla. All rights reserved.')

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Suspense } from "react";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import Earth from "../public/Earth";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Welcome To My 3D World</h1>
+      </div>
+      <Canvas>
+        <ambientLight />
+        <OrbitControls />
+        <Suspense fallback={null}>
+          <Earth />
+        </Suspense>
+        <Environment preset="sunset" />
+      </Canvas>
+      <footer
+        style={{
+          backgroundColor: "#333",
+          color: "#fff",
+          padding: "20px",
+          textAlign: "center",
+        }}
+      >
+        &copy; 2024 Jetnik Syla. All rights reserved.
+      </footer>
+    </>
+  );
+}
+
+export default App;
